6af3404731dbfb455a3ecd178586bfe5b1d7a421
Godswillben <amogugodswill99@gmail.com>
2025-01-15 12:34:28 +0100

    landing page footer componet - Desktop view

---
+++ src/Components/primary/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PrimaryFooter() {
+  return (
+    <div className='font-Elsie text-center tracking-[.4rem] mb-12'>All rights reserved @karuna 2025</div>
+  )
+}
+
+export default PrimaryFooter
