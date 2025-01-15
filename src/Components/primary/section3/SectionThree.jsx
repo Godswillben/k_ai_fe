b1cb0e36eec6f7c36d0e64aff88ac45d9e8c6ab7
Godswillben <amogugodswill99@gmail.com>
2025-01-15 12:33:34 +0100

    landing page section 3 componet - Desktop view

---
+++ src/Components/primary/section3/SectionThree.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PrimarySectionThree() {
+  return (
+    <div className='bg-[#F9F9F9] text-[4rem] px-[20rem] mb-[10rem] py-20 leading-[8rem] tracking-[.5rem] font-Elsie'>Give Your Child an unfair advantage that does not feel like work.</div>
+  )
+}
+
+export default PrimarySectionThree
